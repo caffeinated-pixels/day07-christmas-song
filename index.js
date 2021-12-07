9b2ee186cd1e250983da90739c930b88f0766ea8
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-12-07 08:46:22 -0500

    Add play, pause & stop functionality

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const playBtn = document.getElementById('play-btn')
 const pauseBtn = document.getElementById('pause-btn')
 const stopBtn = document.getElementById('stop-btn')
 
+const xmasAudio = new Audio('./bells.mp3')
+console.log('dave')
+
+playBtn.addEventListener('click', () => xmasAudio.play())
+pauseBtn.addEventListener('click', () => xmasAudio.pause())
+stopBtn.addEventListener('click', () => {
+  xmasAudio.pause()
+  xmasAudio.currentTime = 0
+})
+
+// playBtn.onclick =
+// pauseBtn.onclick = xmasAudio.pause()
+
 // Task:
 // - Add the functionality to play, pause and stop the jingling bells (bells.mp3).
 
